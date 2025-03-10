e65473865c8b9d3aa73a95fd6ca7f72aed657da0
helixoid <ayushrajmth91@gmail.com>
2025-03-10 18:44:42 +0530

    First Commit

---
+++ config/fish/config.fish
@@ -0,0 +1,28 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    theme_gruvbox dark hard
+    starship init fish | source
+    atuin init fish | source
+    export BAT_THEME="gruvbox-dark"
+    
+    # Aliases
+    alias v="nvim"
+    alias prop="hyprctl clients | grep -i 'class\|title\|xwayland'"
+    alias ls='eza --icons=always'
+    alias ff='fastfetch'
+    alias la='eza --icons=always -a'
+    alias lla='eza --icons=always -la'
+    alias lt='eza --icons=always -la --tree'
+    alias ytmd='yt-dlp -x --add-metadata'
+    alias wlp='swww img'
+    
+    # Yazi Integration
+    function y
+        set tmp (mktemp -t "yazi-cwd.XXXXXX")
+        yazi $argv --cwd-file="$tmp"
+        if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+            builtin cd -- "$cwd"
+        end
+        rm -f -- "$tmp"
+    end
+end
